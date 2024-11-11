3b666d333c338715d3dc25dfda56cb69a8d0455b
saboyutaka <yutaka.paradigm.shift@gmail.com>
2024-11-11 16:27:45 +0900

    divide files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
-import { createServer } from 'node:http'
-import { createYoga } from 'graphql-yoga'
-import { schema } from './schema.js'
+import { loadFiles } from "@graphql-tools/load-files"
+import { createServer } from "node:http"
+import { createYoga, createSchema } from "graphql-yoga"
+import { resolvers } from "./resolvers.js"
+
+const schema = createSchema({
+  typeDefs: await loadFiles("src/schema.graphql"),
+  resolvers,
+})
 
 const yoga = createYoga({ schema })
 const server = createServer(yoga)
 
 server.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+  console.info("Server is running on http://localhost:4000/graphql")
 })
